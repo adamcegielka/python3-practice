6793922b346a8600e02ddecfa7b91a43e9c0c79c
Adam Cegiełka <adamcegielka@tlen.pl>
2025-12-27 13:05:26 +0100

    add avarage challenge

---
+++ challenges/02_average_number.py
@@ -0,0 +1,13 @@
+# 1. oblich średnią z następujących lich: 10, 20, 30, 40, 50
+# przypisz obliczenia do zmiennej average, przy obliczeniach uzyj nawiasów
+# 2. pokaż wynik na ekranie używając funkcji print() i napisu "Średnia wynosi: "
+# 3. pokaż typ zmiennej average używając funkcji type() wewnątrz funkcji print()
+
+number1 = 10
+number2 = 20
+number3 = 30
+number4 = 40
+number5 = 50
+average = (number1 + number2 + number3 + number4 + number5) / 5
+print("Średnia wynosi:", average)
+print(type(average))
