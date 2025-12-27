2fc4359e05abec02f96e3530d311466cd8fc6e5a
Adam Cegiełka <adamcegielka@tlen.pl>
2025-12-27 12:35:42 +0100

    add first challenge

---
+++ challenges/variables.py
@@ -0,0 +1,15 @@
+# Zapisz zmiannej z identyfikatorami które opisują widaomość email np. tytuł, nadawcę, odbiorcę, treść wiadomości
+email_title = "Meeting Reminder"
+email_sender = "Manager"
+email_recipient = "Employee"
+email_body = "Don't forget about the meeting tomorrow at 10 AM."
+
+# Wyświetl każdą z tych zmiennych
+print("Email Title:", email_title)
+print("Email Sender:", email_sender)
+print("Email Recipient:", email_recipient)
+print("Email Body:", email_body)
+
+# Zmodyfikuj treść wiadomości, dodając informację o lokalizacji spotkania
+email_body += " The meeting will be held in Conference Room A."
+print("Updated Email Body:", email_body)
