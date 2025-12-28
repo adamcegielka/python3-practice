9b7d713bdb72ee8a5415a5984de52f149fd069f0
Adam Cegiełka <adamcegielka@tlen.pl>
2025-12-28 09:21:09 +0100

    add string challenge

---
+++ challenges/03_string_test.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+surname = input("Enter your surname: ")
+city = input("Enter your city: ")
+full_info = f"Name: {name} {surname}, \nCity: {city}"
+
+print(full_info)
