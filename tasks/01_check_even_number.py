4824ecd679f0c6463cf650a9d885b53c3d553f04
Adam Cegiełka <adamcegielka@tlen.pl>
2025-12-28 18:15:15 +0100

    add even number task

---
+++ tasks/01_check_even_number.py
@@ -0,0 +1,15 @@
+# Check if a number is even or odd
+number = 3
+if number % 2 == 0:
+    print(f"{number} is an even number")
+else:
+    print(f"{number} is an odd number")
+
+# Change the value of number and check again
+number = 10
+if number % 2 == 0:
+    print(f"{number} is an even number")
+else:
+    print(f"{number} is an odd number")
+
+print(number) # 10
