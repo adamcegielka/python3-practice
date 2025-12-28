2bd8fb475e55ee3bf3f98c0a0f2791c3fd490ee0
Adam Cegiełka <adamcegielka@tlen.pl>
2025-12-28 19:21:33 +0100

    add area of rectangle task

---
+++ tasks/06_area_of_rectangle.py
@@ -0,0 +1,9 @@
+# oblicz pole prostokąta po tym, jak użytkownik wprowadzi długość i szerokość
+
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+area = length * width
+
+print(f"The area of the rectangle is: {area}")
+
+# run this code in Python: `Ctrl + Fn + F5`
