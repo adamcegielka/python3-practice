f62def5d4baf0ab54e66de9ac50b636fd6dce9e2
Adam Cegiełka <adamcegielka@tlen.pl>
2025-12-28 09:57:59 +0100

    add boolean challenge

---
+++ challenges/04_boolean.py
@@ -0,0 +1,14 @@
+number1 = 10
+number2 = 20
+
+print("Check if number is positive:", number1 > 0)   # True
+print("Check if number is negative:", number2 < 0)   # False
+print("Check if number is equal to 0", number1 == 0)  # False
+print("Check if numbers are equal:", number1 == number2)  # False
+
+if number1 < number2:
+    print(f"{number1} is less than {number2}")
+else:
+    print(f"{number1} is not less than {number2}")
+
+# run this code in Python: `Ctrl + Fn + F5`
