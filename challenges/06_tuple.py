4b99a7b3ca87a5940c63717670d56004071ac669
Adam Cegiełka <adamcegielka@tlen.pl>
2026-01-10 10:03:22 +0100

    refactor

--- challenges/06_tuple.py
+++ challenges/06_tuple.py
@@ -7,4 +7,4 @@ print(f"Tuple length: {len(numbers)}")  # 5
 print(f"Last item: {numbers[-1]}") # Last item: 250
 print(f"Slicing (1:4): {numbers[1:4]}") # Slicing (1:4): (100, 150, 200)
 print(f"Every second item: {numbers[::2]}")  # Every second item\
-print(f"Third item to end: {numbers[-3]}")  # Third item to end: 150]}")
+print(f"Third item from end: {numbers[-3]}")  # Third item to end: 150
