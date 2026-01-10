7c7081e00746c295bd3907ae98a0c217b413c9aa
Adam Cegiełka <adamcegielka@tlen.pl>
2026-01-10 09:49:45 +0100

    add new task with Tuple

---
+++ challenges/06_tuple.py
@@ -0,0 +1,10 @@
+# # Tuple Examples
+
+numbers = (50, 100, 150, 200, 250)
+print(f"Numbers: {numbers}")  # Numbers: (50, 100, 150, 200, 250)
+print(type(numbers))  # <class 'tuple'>
+print(f"Tuple length: {len(numbers)}")  # 5
+print(f"Last item: {numbers[-1]}") # Last item: 250
+print(f"Slicing (1:4): {numbers[1:4]}") # Slicing (1:4): (100, 150, 200)
+print(f"Every second item: {numbers[::2]}")  # Every second item\
+print(f"Third item to end: {numbers[-3]}")  # Third item to end: 150]}")
